db8f29b8fc834adbb9dec43e8e3ed649cffb2a75
ccq <fdgdshgfjh>
2020-10-23 15:12:26 +0800

    test

---
+++ Assets/Script/Editor/BuildPlayerExample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildPlayerExample : MonoBehaviour
+{
+    [MenuItem("Build/Build Android")]
+    public static void MyBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/Login.unity", "Assets/Scenes/Game.unity" };
+        
+        buildPlayerOptions.locationPathName = "AndroidExportBuild";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.AcceptExternalModificationsToPlayer | BuildOptions.UncompressedAssetBundle ;
+        //buildPlayerOptions.options = BuildOptions.None;
+        
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
---
+++ Assets/Script/Editor/PrefabsBuilder.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class PrefabsBuilder : MonoBehaviour {
+
+	[MenuItem("Build/Build/BuildAllBundles")]
+	public static void BuildAllPrefabs(){
+		string outputPath=Application.dataPath+"/Output/Bundles/"+current;
+		TouchFolder(outputPath);
+		BuildPipeline.BuildAssetBundles(outputPath,BuildAssetBundleOptions.DeterministicAssetBundle,current);
+		AssetDatabase.Refresh();
+	}
+
+	[MenuItem("Assets/Look/Bundle Content")]
+	public static void LookBundleContent(){
+		string path=AssetDatabase.GetAssetPath(Selection.activeObject);
+		path=path.TrimStart("Assets".ToCharArray());
+		path=Application.dataPath+path;
+		WWW fileLoader = new WWW ("file://" + path);
+		if(fileLoader.assetBundle!=null){
+			string[] assetNames=fileLoader.assetBundle.GetAllAssetNames();
+			for(int i=0;i<assetNames.Length;i++){
+				Debug.Log(assetNames[i]);
+				//Object obj=fileLoader.assetBundle.LoadAsset(assetNames[i]);
+			}
+			fileLoader.assetBundle.Unload(true);
+		}
+	}
+
+	private static void TouchFolder(string dir){
+		if(!Directory.Exists(dir)){
+			Directory.CreateDirectory(dir);
+		}
+	}
+	private static void RenameFile(string from,string to){
+		FileUtil.DeleteFileOrDirectory(to);
+		FileUtil.MoveFileOrDirectory(from,to);
+	}
+
+	public static BuildTarget current{
+		get{
+			#if UNITY_ANDROID
+			return BuildTarget.Android;
+			#elif UNITY_IOS
+			return BuildTarget.iOS;
+			#else
+			return BuildTarget.StandaloneWindows;
+			#endif
+		}
+	}
+}
